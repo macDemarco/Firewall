5ecacde4154fc602febb618ca7f341146bcff36d
macDemarco <shevach.tamar@gmail.com>
2016-11-26 18:53:28 +0200

    Update Question7.py

--- Question7.py
+++ Question7.py
@@ -1,16 +1,11 @@
 class LFSR:
-    
+
     def __init__(self, initial_state, hardwiring):
-        if len(initial_state) != len(hardwiring):
-            raise ValueError("initial_state and hardwiring must have the same length")
         self.stages_num = len(initial_state)
         self.state = initial_state
         self.hardwiring = hardwiring
         
     def step(self):
-        '''
-        Makes a step by calculating the feedback bit and shifting.
-        '''
         feedback_bit = 0
         for i in range(0, self.stages_num):
             feedback_bit += self.hardwiring[i] * self.state[i]
@@ -18,9 +13,6 @@ class LFSR:
         self.state = self.state[1:] + [feedback_bit]
         
     def output(self, bits_num):
-        ''' 
-        Prints the first bits_num bits of the output.
-        '''
         print("First " + str(bits_num) + " bits:") 
         for j in range(bits_num):
             print(self.state[0], end = " ")
@@ -28,9 +20,6 @@ class LFSR:
         print()
         
     def printPeriod(self):
-        '''
-        Prints the number of steps it takes until an internal state is repeated.
-        '''
         states = dict()
         i = 0
         while True:
@@ -41,8 +30,7 @@ class LFSR:
                 print("The period is " + str(i) + " steps.")
                 return            
            
-
-def main():
+if __name__ == "__main__":
     print("LFSR1:")
     initial_state = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     hardwiring = [1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -56,5 +44,3 @@ def main():
     LFSR2.output(30)
     LFSR2.printPeriod()
     
-if __name__ == "__main__":
-    main()
